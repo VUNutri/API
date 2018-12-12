8b798e1835d3f9480f70a1f8898032d81cf69cc0
Dominykas Seputis <dom.seputis@gmail.com>
2018-12-12 12:45:59 +0200

    Added cors

--- api/main.go
+++ api/main.go
@@ -10,12 +10,23 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
 	"github.com/go-chi/render"
 )
 
 func routes() *chi.Mux {
+	cors := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+
 	router := chi.NewRouter()
 	router.Use(
+		cors.Handler
 		render.SetContentType(render.ContentTypeJSON),
 		middleware.Logger,
 		middleware.DefaultCompress,
